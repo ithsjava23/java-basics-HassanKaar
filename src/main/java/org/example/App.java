be25a0f92418ce246cee319d09e663c492fc02e9
Hassan Ali Guuled <143558667+HassanKaar@users.noreply.github.com>
2023-09-17 21:16:53 +0200

    Add files via upload

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,144 +1,133 @@
 package org.example;
 
-import java.util.Arrays;
 import java.util.Locale;
 import java.util.Scanner;
 
 
-    public class App {
-        public static void main(String[] args) {
-            Scanner input = new Scanner(System.in);
-            Locale swedishLocale;
-            swedishLocale = new Locale ("sv", "SE");
-            Locale.setDefault(swedishLocale);
-            int[] ePrices = new int[24];
-            boolean slide;
-            slide = true;
+public class App {
+    public static void main(String[] args) {
 
-            while (slide) {
-                System.out.println("Elpriser");
-                System.out.println("========");
-                System.out.println("1. Inmatning");
-                System.out.println("2. Min, Max och Medel");
-                System.out.println("3. Sortera");
-                System.out.println("4. Bästa Laddningstid (4h)");
-                System.out.println("e. Avsluta");
+        Scanner scanner = new Scanner(System.in);
+        Locale swedishLocale = new Locale("sv", "SE");
+        Locale.setDefault(swedishLocale);
+        int[] prices = new int[24];
+        String choice;
+
+        do {
+            System.out.print(
+                    """
+                   Elpriser
+                   ========
+                   1. Inmatning
+                   2. Min, Max och Medel
+                   3. Sortera
+                   4. Bästa Laddningstid (4h)
+                   e. Avsluta
+                     """);
 
 
-                String option = input.next();
+            choice = scanner.nextLine();
+            choice = choice.toLowerCase();
 
-                if (option.equalsIgnoreCase("1"))
-                    inputElpriser(ePrices, input);
-                else if (option.equalsIgnoreCase("2"))
-                    minMaxMedel (ePrices);
-                else if (option.equalsIgnoreCase("3"))
-                    sorteraElpriser(ePrices);
-                else if (option.equalsIgnoreCase("4"))
-                    bestChargingTime(ePrices);
-                else if (option.equalsIgnoreCase("e"))
-                    slide = false;
-                else
-                    System.out.println("Ogiltigt val. Försök igen.");
-
+            if (choice.equals("1")) {
+                ElPrices(prices, scanner);
+            } else if (choice.equals("2")) {
+                MinMaxMedel(prices);
+            } else if (choice.equals("3")) {
+                Sort(prices);
+            } else if (choice.equals("4")) {
+                BestChargingTime(prices);
+            } else if (choice.equals("e")) {
+                System.out.print("Programmet avslutas.");
             }
-
-            System.out.println("Programmet avslutas.");
-        }
-
-
-        private static void inputElpriser(int[] elpriser, Scanner input) {
-            System.out.println("Ange dygnets pris i öre per intervall (2h):");
-
-            int i = 0;
-            int timme = 0;
-
-            while (i < elpriser.length) {
-                int nastaTimme = (timme + 1) % 24;
-
-                System.out.printf("%02d-%02d: ", timme, nastaTimme);
-                elpriser[i] = input.nextInt();
-
-                i++;
-                timme = nastaTimme;
-            }
-        }
-
-        public static void minMaxMedel(int[] prices) {
-            int minPrice = Integer.MAX_VALUE;
-            int maxPrice = Integer.MIN_VALUE;
-            int minTim = -1;
-            int maxTim = -1;
-            int total = 0;
-
-            for (int tim = 0; tim < 24; tim++) {
-                int price = prices[tim];
-                total += price;
-
-                if (price < minPrice) {
-                    minPrice = price;
-                    minTim = tim;
-                }
-
-                if (price > maxPrice) {
-                    maxPrice = price;
-                    maxTim = tim;
-                }
-            }
-
-            double average = (double) total / prices.length;
-
-            System.out.println("Lägsta pris: " + minPrice + " öre per kW/h (kl " + minTim + ")");
-            System.out.println("Högsta pris: " + maxPrice + " öre per kW/h (kl " + maxTim + ")");
-            System.out.println("Medelpris: " + average + " öre");
-
-            System.out.println("Timmar med lägsta pris:");
-            for (int i = 0; i < prices.length; i++) {
-                if (prices[i] == minPrice) {
-                    System.out.print(i + "-" + (i + 1) % 24 + " ");
-                }
-            }
-            System.out.println();
-
-            System.out.println("Timmar med högsta pris:");
-            for (int i = 0; i < prices.length; i++) {
-                if (prices[i] == maxPrice) {
-                    System.out.print(i + "-" + (i + 1) % 24 + " ");
-                }
-            }
-            System.out.println();
-        }
-
-
-        private static void sorteraElpriser(int[] elpriser) {
-            int[] kopieradePriser = Arrays.copyOf(elpriser, elpriser.length);
-            Arrays.sort(kopieradePriser);
-
-            System.out.println("Sortering från dyrast till billigast:");
-            for (int i = elpriser.length - 1; i >= 0; i--) {
-                System.out.printf("%02d-%02d %d öre%n", i, i + 1, kopieradePriser[i]);
-            }
-        }
-
-        private static void bestChargingTime(int[] elpriser) {
-            int[] kopieradePriser = Arrays.copyOf(elpriser, elpriser.length);
-            int billigasteTotalPris = Integer.MAX_VALUE;
-            int startTimme = 0;
-
-            for (int i = 0; i <= elpriser.length - 4; i++) {
-                int totalPris = kopieradePriser[i] + kopieradePriser[i + 1] + kopieradePriser[i + 2] + kopieradePriser[i + 3];
-                if (totalPris < billigasteTotalPris) {
-                    billigasteTotalPris = totalPris;
-                    startTimme = i;
-                }
-            }
-
-            int slutTimme = startTimme + 3;
-            int medelPris = billigasteTotalPris / 4;
-
-            System.out.println("Bästa laddningstid (4h):");
-            System.out.println("Påbörja laddning kl: " + startTimme + ":00 - Sluttid: " + slutTimme + ":59");
-            System.out.println("Medelpris 4h: " + medelPris + " öre per kW/h");
-        }
+        } while (!choice.equals("e"));
     }
 
+    public static void ElPrices(int[] prices, Scanner scanner) {
+        System.out.print("Ange dygnets pris i öre per intervall (2h)\n");
 
+        for (int tim = 0; tim < 24; tim++) {
+            System.out.print((tim) + "-" + (tim + 1) + ": ");
+            prices[tim] = scanner.nextInt();
+            scanner.nextLine();
+        }
+
+    }
+
+    public static void MinMaxMedel(int[] prices) {
+        int minPrice = Integer.MAX_VALUE;
+        int maxPrice = Integer.MIN_VALUE;
+        int minTim = -1;
+        int maxTim = -1;
+        int sum = 0;
+
+        for (int tim = 0; tim < 24; tim++) {
+            int price = prices[tim];
+            if (price < minPrice) {
+                minPrice = price;
+                minTim = tim;
+            }
+
+            if (price > maxPrice) {
+                maxPrice = price;
+                maxTim = tim;
+            }
+            sum += price;
+        }
+        float medel;
+        medel = (float) sum / prices.length;
+
+        System.out.printf("Lägsta pris: %02d-%02d, %d öre/kWh\n", minTim, minTim + 1, minPrice);
+        System.out.printf("Högsta pris: %02d-%02d, %d öre/kWh\n", maxTim, maxTim + 1, maxPrice);
+        System.out.printf("Medelpris: %.2f öre/kWh\n", medel);
+    }
+
+    public static void BestChargingTime(int[] prices) {
+        int lowestPrice = Integer.MAX_VALUE;
+        int bestStartTime = -1;
+
+        for (int startTime = 0; startTime < 21; startTime++) {
+            int totalPrice = 0;
+            for (int i = startTime; i < startTime + 4; i++) {
+                totalPrice += prices[i];
+            }
+            if (totalPrice < lowestPrice) {
+                lowestPrice = totalPrice;
+                bestStartTime = startTime;
+            }
+        }
+        float medelValue = (float) lowestPrice / 4;
+
+        System.out.printf("Påbörja laddning klockan %02d\n", bestStartTime, +3);
+        System.out.printf("Medelpris 4h: %.1f öre/kWh\n", medelValue);
+    }
+
+    public static void Sort(int[] prices) {
+        int[] hours = new int[24];
+        for (int i = 0; i < 24; i++) {
+            hours[i] = i;
+        }
+        boolean sorted = false;
+
+        while (!sorted) {
+            sorted = true;
+            for (int i = 0; i < 23; i++) {
+                if (prices[i] < prices[i + 1]) {
+
+                    int tempPrice = prices[i];
+                    prices[i] = prices[i + 1];
+                    prices[i + 1] = tempPrice;
+
+                    int tempHour = hours[i];
+                    hours[i] = hours[i + 1];
+                    hours[i + 1] = tempHour;
+
+                    sorted = false;
+                }
+            }
+        }
+        for (int i = 0; i < 24; i++) {
+            System.out.printf("%02d-%02d %d öre\n", hours[i], hours[i] + 1, prices[i]);
+        }
+    }
+}
